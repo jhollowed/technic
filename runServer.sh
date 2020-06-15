12c208bfc5ad785271f5d1f0e9c431f8ead7e464
Joe Hollowed <jphollowed@gmail.com>
2020-06-15 16:08:31 -0500

    automated commit from runServer.sh

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-$JAVA=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java
+JAVA=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java
 $JAVA -Xmx2G -Xms2G -jar Thermos-1.7.10-1448-57-server.jar nogui
 git add .
 git commit -am "automated commit from runServer.sh"
