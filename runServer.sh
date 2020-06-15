a233caa4f358f13a285695fef54f03751ed93ecf
Joe Hollowed <jphollowed@gmail.com>
2020-06-15 16:01:07 -0500

    automated commit from runServer.sh

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-java -Xmx1024M -Xms1024M -jar Thermos-1.7.10-1448-57-server.jar nogui
+java -Xmx2G -Xms2G -jar Thermos-1.7.10-1448-57-server.jar nogui
 git add .
 git commit -am "automated commit from runServer.sh"
 git push
